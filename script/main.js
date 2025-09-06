4f5d67b22d5386e7774ffac8ab66fadcd3123d06
Nik <nik_sn_fr_end@outlook.com>
2025-09-06 20:37:03 +0300

    finish desctop

--- script/main.js
+++ script/main.js
@@ -1,25 +1,30 @@
-// slider
+
+const openSearchBtn = document.getElementById('openSearch');
+const form = document.querySelector('.header__form');
+
+openSearchBtn.addEventListener('click', () => {
+    form.classList.toggle('active');
+});
+
+
+
 
 const swiper = new Swiper('.swiper', {
     effect: 'cube',
+    cubeEffect: {
+        shadow: false,
+        slideShadows: false,
+    },
     slidesPerView: 1,
-    speed: 800,
+    speed: 1100,
     loop: true,
-
-
     pagination: {
         el: '.store__pagination',
         type: 'bullets',
         clickable: true,
     },
-
-
-
     navigation: {
         nextEl: '.store__next',
         prevEl: '.store__prev',
     },
-
-
-
 });
